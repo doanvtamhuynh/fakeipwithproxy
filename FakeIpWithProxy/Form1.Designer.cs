549d4946e30112daab1d4c968c37e3af7c81e77b
doanvtamhuynh <huynhdoan.2132@gmail.com>
2024-08-05 19:34:44 +0700

    feat: add proxy

--- FakeIpWithProxy/Form1.Designer.cs
+++ FakeIpWithProxy/Form1.Designer.cs
@@ -39,15 +39,17 @@
             this.btnStop = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtType = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtProxy = new System.Windows.Forms.TextBox();
+            this.cbbTypeProxy = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // txtIpProxy
             // 
-            this.txtIpProxy.Location = new System.Drawing.Point(36, 58);
+            this.txtIpProxy.Location = new System.Drawing.Point(35, 225);
             this.txtIpProxy.Margin = new System.Windows.Forms.Padding(4);
             this.txtIpProxy.Name = "txtIpProxy";
-            this.txtIpProxy.Size = new System.Drawing.Size(316, 27);
+            this.txtIpProxy.Size = new System.Drawing.Size(316, 23);
             this.txtIpProxy.TabIndex = 0;
             // 
             // label1
@@ -55,10 +57,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Location = new System.Drawing.Point(28, 190);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "IP Proxy";
             // 
@@ -67,19 +69,19 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(425, 23);
+            this.label2.Location = new System.Drawing.Point(424, 190);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.Size = new System.Drawing.Size(90, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Port Proxy";
             // 
             // txtPortProxy
             // 
-            this.txtPortProxy.Location = new System.Drawing.Point(432, 58);
+            this.txtPortProxy.Location = new System.Drawing.Point(431, 225);
             this.txtPortProxy.Margin = new System.Windows.Forms.Padding(4);
             this.txtPortProxy.Name = "txtPortProxy";
-            this.txtPortProxy.Size = new System.Drawing.Size(316, 27);
+            this.txtPortProxy.Size = new System.Drawing.Size(316, 23);
             this.txtPortProxy.TabIndex = 2;
             // 
             // label3
@@ -87,19 +89,19 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(425, 108);
+            this.label3.Location = new System.Drawing.Point(424, 265);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.Size = new System.Drawing.Size(86, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(432, 143);
+            this.txtPassword.Location = new System.Drawing.Point(431, 300);
             this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(316, 27);
+            this.txtPassword.Size = new System.Drawing.Size(316, 23);
             this.txtPassword.TabIndex = 6;
             // 
             // label4
@@ -107,25 +109,25 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(29, 108);
+            this.label4.Location = new System.Drawing.Point(28, 265);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 25);
+            this.label4.Size = new System.Drawing.Size(91, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Username";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(36, 143);
+            this.txtUsername.Location = new System.Drawing.Point(35, 300);
             this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(316, 27);
+            this.txtUsername.Size = new System.Drawing.Size(316, 23);
             this.txtUsername.TabIndex = 4;
             // 
             // btnStop
             // 
             this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStop.Location = new System.Drawing.Point(36, 307);
+            this.btnStop.Location = new System.Drawing.Point(36, 383);
             this.btnStop.Margin = new System.Windows.Forms.Padding(4);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(317, 49);
@@ -137,7 +139,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(430, 307);
+            this.button1.Location = new System.Drawing.Point(430, 383);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(317, 49);
@@ -150,28 +152,55 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(361, 206);
+            this.label5.Location = new System.Drawing.Point(338, 103);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.Size = new System.Drawing.Size(95, 20);
             this.label5.TabIndex = 11;
-            this.label5.Text = "Type";
+            this.label5.Text = "Type Proxy";
             // 
-            // txtType
+            // label6
             // 
-            this.txtType.Location = new System.Drawing.Point(244, 235);
-            this.txtType.Margin = new System.Windows.Forms.Padding(4);
-            this.txtType.Name = "txtType";
-            this.txtType.Size = new System.Drawing.Size(316, 27);
-            this.txtType.TabIndex = 10;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(338, 18);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Your Proxy";
+            // 
+            // txtProxy
+            // 
+            this.txtProxy.Location = new System.Drawing.Point(37, 55);
+            this.txtProxy.Margin = new System.Windows.Forms.Padding(4);
+            this.txtProxy.Name = "txtProxy";
+            this.txtProxy.Size = new System.Drawing.Size(710, 23);
+            this.txtProxy.TabIndex = 13;
+            this.txtProxy.TextChanged += new System.EventHandler(this.txtProxy_TextChanged);
+            // 
+            // cbbTypeProxy
+            // 
+            this.cbbTypeProxy.FormattingEnabled = true;
+            this.cbbTypeProxy.Items.AddRange(new object[] {
+            "Socks5",
+            "Http",
+            "Https"});
+            this.cbbTypeProxy.Location = new System.Drawing.Point(245, 137);
+            this.cbbTypeProxy.Name = "cbbTypeProxy";
+            this.cbbTypeProxy.Size = new System.Drawing.Size(266, 25);
+            this.cbbTypeProxy.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 418);
+            this.ClientSize = new System.Drawing.Size(788, 476);
+            this.Controls.Add(this.cbbTypeProxy);
+            this.Controls.Add(this.txtProxy);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtType);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.label3);
@@ -204,7 +233,9 @@
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtProxy;
+        private System.Windows.Forms.ComboBox cbbTypeProxy;
     }
 }
 
--- FakeIpWithProxy/Form1.cs
+++ FakeIpWithProxy/Form1.cs
@@ -15,6 +15,7 @@ namespace FakeIpWithProxy
         public Form1()
         {
             InitializeComponent();
+            cbbTypeProxy.SelectedIndex = 0;
         }
 
         private void btnStop_Click(object sender, EventArgs e)
@@ -23,5 +24,34 @@ namespace FakeIpWithProxy
             stop.StopFakeIp();
         }
 
+        private void txtProxy_TextChanged(object sender, EventArgs e)
+        {
+            txtIpProxy.Text = "";
+            txtPortProxy.Text = "";
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            string proxy = txtProxy.Text;
+            string[] splitProxy = proxy.Split(':');
+            if(splitProxy.Length == 1 )
+            {
+                txtIpProxy.Text = splitProxy[0];
+            }else if(splitProxy.Length == 2)
+            {
+                txtIpProxy.Text = splitProxy[0];
+                txtPortProxy.Text = splitProxy[1];
+            }else if (splitProxy.Length == 3)
+            {
+                txtIpProxy.Text = splitProxy[0];
+                txtPortProxy.Text = splitProxy[1];
+                txtUsername.Text = splitProxy[2];
+            }
+            else
+            {
+                txtIpProxy.Text = splitProxy[0];
+                txtPortProxy.Text = splitProxy[1];
+                txtUsername.Text = splitProxy[2];
+                txtPassword.Text = splitProxy[3];
+            }
+        }
     }
 }
